d3b21e2097f7abf77765a13525cc9e41f595f250
jashema_panapa <jcpanapa@gmail.com>
2020-09-09 11:57:26 -0400

    modified:   Gemfile 	new file:   config/webpacker.yml 	modified:   db/migrate/04_create_comments.rb

--- db/migrate/04_create_comments.rb
+++ db/migrate/04_create_comments.rb
@@ -1,9 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
     def change
       create_table :comments do |t|
-        t.string :username
-        t.string :email 
-        t.string :password_digest
+        t.string :body
+        
         
       end
    end
