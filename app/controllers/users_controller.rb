3fb4f8a5167e4033e4b376c0d299dcaa2368c9c6
jashema_panapa <jcpanapa@gmail.com>
2020-11-04 11:18:32 -0500

    modified:   app/controllers/application_controller.rb 	modified:   app/controllers/users_controller.rb 	new file:   app/views/welcome.html.erb 	modified:   config/routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,38 @@
 class UsersController < ApplicationController
-    
+    skip_before_action :verified_user, only: [:new, :create]
+
+    def signup
+        #if logged_in?
+         redirect to 'businesses' 
+       end
+   
+       render 'signup' 
+    end
+
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    if (user = User.create(user_params))
+      session[:user_id] = user.id
+      redirect_to "/"
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    #show users all businesses?
+  end
+
+  private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
 
 end  
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.
+  resources :users
+  resources :businesses
+  resources :comments
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+
 end
