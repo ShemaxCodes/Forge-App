e0e5274e59f443e261b8dd01fbb327c2c19c71ee
jashema_panapa <jcpanapa@gmail.com>
2020-12-05 13:48:26 -0500

    Fixed log out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,7 +27,7 @@ end
 
 
 def destroy
-    log_out if logged_in?
+    log_out #if logged_in?
     redirect_to '/'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new", as: 'login_path'
   post "/login", to: "sessions#create"
   delete '/logout', to: "sessions#destroy", as: 'logout_path'
+  get '/logout', to: "sessions#destroy"
+  resources :sessions
 
   
 
